1fd538e512acb816c7604821dd4f30030b0fca20
Henry <mhenry2406@gmail.com>
2025-06-21 12:33:38 -0600

    Primer commit

---
+++ Controllers/CrearEventosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Proyecto_Compromiso_Social.Controllers
+{
+    public class CrearEventosController : Controller
+    {
+        public IActionResult CrearEvento()
+        {
+            return View();
+        }
+    }
+}
