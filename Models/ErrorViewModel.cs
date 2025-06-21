a206e853f0a2a4153440714afa4c6eb045198d1f
Henry <mhenry2406@gmail.com>
2025-06-21 14:33:07 -0600

    Se agrega la API

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Proyecto_Compromiso_Social.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
