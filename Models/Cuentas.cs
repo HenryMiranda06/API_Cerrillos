8d6e3e0504e2fb18e6db80b4aa09e022d080a056
Henry <mhenry2406@gmail.com>
2025-06-21 14:35:43 -0600

    Ahora sí se sube la api xd

---
+++ Models/Cuentas.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_Cerrillos.Models
+{
+    public class Cuentas
+    {
+        [Key]
+        public string Correo { get; set; }
+
+        public string Contraseña { get; set; }
+    }
+}
